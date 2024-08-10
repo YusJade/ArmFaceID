1d596085ed5a5382f2a2e5f664ce834a61f18202
YuXeng <1917912744@qq.com>
2024-08-10 16:18:09 +0800

    intro SeetaFace2 into server

--- server/rpc_manager.cpp
+++ server/rpc_manager.cpp
@@ -2,18 +2,39 @@
 
 #include <vector>
 
+#include "utils/utils.h"
+
+arm_face_id::RpcManagerImpl::RpcManagerImpl() {
+  seeta::ModelSetting FD_model_setting("FD_model_path",
+                                       seeta::ModelSetting::Device::CPU);
+  seeta::ModelSetting PD_model_setting("PD_model_path",
+                                       seeta::ModelSetting::Device::CPU);
+  seeta::ModelSetting FR_model_setting("FD_model_path",
+                                       seeta::ModelSetting::Device::CPU);
+}
+
 grpc::Status arm_face_id::RpcManagerImpl::RecognizeFace(
     grpc::ServerContext* context, const arm_face_id::RecognizeRequest* request,
     arm_face_id::RecognizeResult* response) {
   // 测试内容：返回一张照片
-  cv::Mat test_image = cv::imread("./LDH.jpg");
-  std::vector<uchar> image_encoded;
-  cv::imencode(".jpg", test_image, image_encoded);
-  std::string bytes(image_encoded.begin(), image_encoded.end());
+  // cv::Mat test_image = cv::imread("./LDH.jpg");
+  // std::vector<uchar> image_encoded;
+  // cv::imencode(".jpg", test_image, image_encoded);
+  // std::string bytes(image_encoded.begin(), image_encoded.end());
   // response->set_face_img(bytes);
-  // 直接返回请求里的图片
-  response->set_face_img(request->face_img());
-  response->set_id("1");
-  response->set_name("LDH");
+  // // 直接返回请求里的图片
+  // response->set_face_img(request->face_img());
+  // response->set_id("1");
+  // response->set_name("LDH");
+  cv::Mat img_decoded;
+  utils::decodeMat(request->face_img(), img_decoded);
+  SeetaImageData seeta_image_data;
+  seeta_image_data.channels = img_decoded.channels();
+  seeta_image_data.width = img_decoded.cols;
+  seeta_image_data.height = img_decoded.rows;
+  seeta_image_data.data = img_decoded.data;
+
+  float similarity = 0.0;
+  int64_t faces = face_engine_ptr->Query(seeta_image_data, &similarity);
   return grpc::Status::OK;
 }
--- server/rpc_manager.h
+++ server/rpc_manager.h
@@ -5,6 +5,8 @@
 #include <grpcpp/support/status.h>
 #include <opencv2/opencv.hpp>
 
+#include "face_engine.h"
+
 #include "face_network.grpc.pb.h"
 #include "face_network.pb.h"
 
@@ -12,9 +14,14 @@ namespace arm_face_id {
 
 class RpcManagerImpl final : public arm_face_id::RpcManager::Service {
  public:
+  RpcManagerImpl();
+
   ::grpc::Status RecognizeFace(::grpc::ServerContext* context,
                                const arm_face_id::RecognizeRequest* request,
                                arm_face_id::RecognizeResult* response);
+
+ private:
+  std::unique_ptr<::seeta::FaceEngine> face_engine_ptr;
 };
 
 }  // namespace arm_face_id
