207a927e727bb5e5a9a46212a82550bee64d339e
serendipity <976758426@qq.com>
2024-11-05 20:20:29 +0800

    配置

--- script/env_configure.sh
+++ script/env_configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SeetaFace6_DIR=/home/yu/codefield/SeetaFace6/SeetaFace6
+export SeetaFace6_DIR=/home/serenNan/codefield/SeetaFace6/SeetaFace6
 
 # export SeetaFace6_DIR=/home/yu/codefield/SeetaFace6/SeetaFace6
 export LD_LIBRARY_PATH=$SeetaFace6_DIR/lib64/:$LD_LIBRARY_PATH
